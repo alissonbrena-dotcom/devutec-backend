ce4018c9bef492a335fc0108b528b5c2f3e12a0c
Ariana Isla <220551972+aisla9@users.noreply.github.com>
2026-09-12 15:56:09 -0500

    Initial project setup: Spring Boot + PostgreSQL + Docker

---
+++ src/main/java/pe/edu/utec/devutec/DevutecApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.utec.devutec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevutecApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevutecApplication.class, args);
+    }
+
+}
