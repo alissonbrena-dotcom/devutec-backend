3947bde975b0af6ef892bbd7ce7db6253eb13d88
Nicole <nicole.yucra.castro@gmail.com>
2026-09-21 14:07:59 -0500

    feat: agregar DTOs de registro y login con validaciones

---
+++ src/main/java/pe/edu/utec/devutec/auth/dto/RegisterRequest.java
@@ -0,0 +1,29 @@
+package pe.edu.utec.devutec.auth.dto;
+
+import pe.edu.utec.devutec.auth.domain.Role;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RegisterRequest {
+
+    @NotBlank
+    private String nombre;
+
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    @Size(min = 8, max = 100)
+    private String password;
+
+    @NotNull
+    private Role rol;
+
+}
