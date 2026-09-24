5f5d01de7b672dc0ead95cf08b7f525cc79422c5
Ariana Isla <220551972+aisla9@users.noreply.github.com>
2026-09-24 16:33:23 -0500

    Notificar al freelancer real cuando se libera el pago

--- src/main/java/pe/edu/utec/devutec/repository/ContractRepository.java
+++ src/main/java/pe/edu/utec/devutec/repository/ContractRepository.java
@@ -1,13 +1,18 @@
 package pe.edu.utec.devutec.repository;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pe.edu.utec.devutec.model.contract.Contract;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ContractRepository extends JpaRepository<Contract, Long> {
 
     List<Contract> findByApplication_Freelancer_User_Id(Long userId);
 
     List<Contract> findByApplication_Project_ClientId(Long clientId);
+
+    @EntityGraph(attributePaths = {"application.project", "application.freelancer.user"})
+    Optional<Contract> findByPayment_Id(Long paymentId);
 }
